85c931e774a7e1c9f8f6118f8568a3821f1e9472
prachi69 <prachichhapariya2000@gmail.com>
2023-10-30 17:40:28 +0530

    commit - api integration in header.js and section1.js

--- frontend/src/components/section1.js
+++ frontend/src/components/section1.js
@@ -1,80 +1,73 @@
-
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import Swiper from 'swiper';
 import './section1.css';
 
-let Data = [
-  {
-    Id: 1,
-    Palace: "Rajendra Nagar",
-    Ground: "90 X 90",
-    Amenities: "food, bev ",
-    Img: "assets/59.jpg",
-  },
-  {
-    Id: 2,
-    Palace: "Vijay Nagar",
-    Ground: "90 X 90",
-    Amenities: "food, bev ",
-    Img: "assets/turf-img.jpg",
-  },
-  {
-    Id: 3,
-    Palace: "Gandhi Nagar",
-    Ground: "90 X 90",
-    Amenities: "food, bev ",
-    Img: "assets/football-ground-flooring.jpg",
-  },
-  {
-    Id: 4,
-    Palace: "IT Park",
-    Ground: "90 X 90",
-    Amenities: "food, bev ",
-    Img: "assets/football-ground-flooring.jpg",
-  },
-];
-
 function Section1() {
+  const [branchData, setBranchData] = useState([]);
+
   useEffect(() => {
     // Inside your React component's useEffect hook
-const mySwiper = new Swiper(".mySwiper", {
-  slidesPerView: 'auto',
-  spaceBetween: 10,
-  centeredSlides: true,
-  freeMode: true,
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  noSwiping:true,
-  noSwipingClass: "swiper-no-swiping",
-  breakpoints: {
-    767: {
-      slidesPerView: 3,
+
+    // Fetch data from the API
+    const fetchBranchData = async () => {
+      try {
+        const response = await fetch(
+          'http://localhost:8080/admin/active/branch'
+        );
+        if (!response.ok) {
+          throw new Error('HTTP error: ' + response.status);
+        }
+        const data = await response.json();
+        setBranchData(data.data);
+      } catch (error) {
+        console.error('Error fetching branch data:', error.message);
+      }
+    };
+
+    fetchBranchData();
+
+    // Initialize the Swiper after data is fetched
+    const mySwiper = new Swiper('.mySwiper', {
+      slidesPerView: 'auto',
       spaceBetween: 10,
-    },
-    576: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-    // Add more breakpoints if needed
-  },
-});
+      centeredSlides: true,
+      freeMode: true,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      noSwiping: true,
+      noSwipingClass: 'swiper-no-swiping',
+      breakpoints: {
+        767: {
+          slidesPerView: 3,
+          spaceBetween: 10,
+        },
+        576: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        // Add more breakpoints if needed
+      },
+    });
 
-mySwiper.allowTouchMove = false;
+    mySwiper.allowTouchMove = false;
 
-// Event listener for the Next button
-document.getElementById("swiper-button-next").addEventListener("click", function () {
-  mySwiper.slideNext();
-});
+    // Event listener for the Next button
+    document
+      .getElementById('swiper-button-next')
+      .addEventListener('click', function () {
+        mySwiper.slideNext();
+      });
 
-// Event listener for the Previous button
-document.getElementById("swiper-button-prev").addEventListener("click", function () {
-  mySwiper.slidePrev();
-});
-
+    // Event listener for the Previous button
+    document
+      .getElementById('swiper-button-prev')
+      .addEventListener('click', function () {
+        mySwiper.slidePrev();
+      });
   }, []); // Run this effect only once
 
   return (
@@ -84,7 +77,8 @@ document.getElementById("swiper-button-prev").addEventListener("click", function
           <div className="slider-sec2-heading">
             <p className="ex-p">EXCLUSIVELY</p>
             <p className="works-p">
-              <span style={{ color: "purple", fontWeight: "bold" }}>works</span> with
+              <span style={{ color: 'purple', fontWeight: 'bold' }}>works</span>{' '}
+              with
             </p>
             <p className="start-p">Startups and founders</p>
           </div>
@@ -92,23 +86,67 @@ document.getElementById("swiper-button-prev").addEventListener("click", function
         <div className="col-md-12 col-sm-12 col-lg-12">
           <div className="mySwiper">
             <div className="swiper-wrapper">
-              {Data.map((data) => (
-                <div className="swiper-slide" key={data.Id}>
+              {branchData.map((data) => (
+                <div className="swiper-slide" key={data.ID}>
                   <Carousel slide={false}>
                     <Carousel.Item>
-                      <div className="content-med">
-                        <div className="swiper-avatar">
-                          <img src={data.Img} alt={data.Palace} />
+                      <div class="content-med ">
+                        <div class="swiper-avatar">
+                          <img
+                            src={`http://localhost:8080/admin/branch/image/active/${data.ID}`}
+                            alt={data.Turf_name}
+                          />
                         </div>
-                        <div className="cites-box">
-                          <h2 className="cite">{data.Palace}</h2>
-                          <p className="cite-box-parag">Ground Size - {data.Ground}</p>
-                          <p className="cite-box-parag">Amenities - {data.Amenities}</p>
-                          <button className="cite1">
-                            <a href="#" className="btn-link">
-                              Book Now →
+                        <div class="cites-box">
+                          <h2 class="cite"> {data.Turf_name}</h2>
+                          <p className="cite-box-parag">{data.Branch_name}</p>
+                          <p class="cite-box-parag">
+                            <i
+                              class="	fas fa-map-marker-alt"
+                              style={{ color: 'red' }}
+                            >
+                              <span
+                                class="address"
+                                style={{ color: 'black', paddingLeft: '10px' }}
+                              >
+                                {data.Branch_address}
+                              </span>
+                            </i>
+                          </p>
+                          <button class="cite1">
+                            <a href="#" class="btn-link">
+                              Book Now{' '}
                             </a>
                           </button>
+                        </div>
+                        <div class="sports-icon">
+                          <span class="material-symbols-outlined tennis">
+                            <img
+                              class="sports-img"
+                              src="assets/batminton.png"
+                            />
+                          </span>
+                          <span class="material-symbols-outlined cricket">
+                            <img class="sports-img" src="assets/447875.png" />
+                          </span>
+                          <span class="material-symbols-outlined basketball">
+                            <img
+                              class="sports-img"
+                              src="assets/footballllll.jpeg"
+                            />
+                          </span>
+                          <span class="material-symbols-outlined soccer">
+                            <img
+                              class="sports-img"
+                              src="assets/fotbal123.png"
+                            />
+                          </span>
+                          <span class="material-symbols-outlined soccer">
+                            <img
+                              class="sports-img"
+                              src="assets/tabletennis.png"
+                            />
+                          </span>
                         </div>
                       </div>
                     </Carousel.Item>
@@ -126,4 +164,3 @@ document.getElementById("swiper-button-prev").addEventListener("click", function
 }
 
 export default Section1;
-
