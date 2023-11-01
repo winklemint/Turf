9cc9bd1915ff91ba2e8ed45b16bd7918eced55ff
winklemint <swatichouhan39@gmail.com>
2023-11-01 13:58:27 +0530

    feat- gitign0re updated

---
+++ backend/config/syncDB.go
@@ -0,0 +1,18 @@
+package config
+
+import "turf/models"
+
+func SyncDB() {
+	DB.AutoMigrate(&models.User{})
+	DB.AutoMigrate(&models.Time_Slot{})
+	DB.AutoMigrate(&models.Turf_Bookings{})
+	DB.AutoMigrate(&models.Package{})
+	DB.AutoMigrate(&models.Package_slot_relationship{})
+	DB.AutoMigrate(&models.Admin{})
+	DB.AutoMigrate(&models.Confirm_Booking_Table{})
+	DB.AutoMigrate(models.Screenshot{})
+	DB.AutoMigrate(models.Branch_info_management{})
+	DB.AutoMigrate(models.Testi_Monial{})
+	DB.AutoMigrate(&models.Content{})
+	DB.AutoMigrate(&models.Carousel{})
+}
