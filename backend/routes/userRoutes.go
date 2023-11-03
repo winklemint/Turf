0da04da690a8a0b9d3d21d7a0f273ee055dc2e73
winklemint <swatichouhan39@gmail.com>
2023-11-03 19:22:22 +0530

    c0mmit - dashb0ard and c00kie m0ds

--- backend/routes/userRoutes.go
+++ backend/routes/userRoutes.go
@@ -16,7 +16,7 @@ func RegisterUserRoutes(router *gin.Engine) {
 		userRoutes.POST("/booking", middleware.RequireUserAuth, controllers.Booking)
 		//userRoutes.POST("/available/slot", controllers.AvailableSlot)
 		userRoutes.PUT("/update", middleware.RequireUserAuth, controllers.UpdateUser)
-		userRoutes.POST("/upload", middleware.RequireUserAuth,controllers.Screenshot)
+		userRoutes.POST("/upload", middleware.RequireUserAuth, controllers.Screenshot)
 		userRoutes.GET("/get/detail", controllers.GetAllDetail)
 		userRoutes.GET("/get/booking/detail", controllers.GetBookingDetail)
 		userRoutes.POST("/get/avl/slots", controllers.Get_Available_slots)
