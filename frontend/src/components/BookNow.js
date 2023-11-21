2c53bc799ab6522908bd44d1f4fd03af49615248
Kishore Gurjar <148521926+vishalpatil8727@users.noreply.github.com>
2023-11-21 12:45:33 +0530

    commit bookingpage desing

--- frontend/src/components/BookNow.js
+++ frontend/src/components/BookNow.js
@@ -1,117 +1,3 @@
-
-// import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
-// import DatePicker from "react-datepicker";
-// import "react-datepicker/dist/react-datepicker.css";
-// import "./BookNow.css";
-// import SlotBooking from "./Booking/SlotBooking";
-
-// function BookingForm() {
-//   const [selectedDate, setSelectedDate] = useState(null);
-//   const [dropdownOptions, setDropdownOptions] = useState([]);
-//   const [selectedOption, setSelectedOption] = useState("");
-//   const [availableSlots, setAvailableSlots] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [showSlotBooking, setShowSlotBooking] = useState(false); // Add state to control visibility
-
-//   const Postdata = () => {
-//     fetch("http://127.0.0.1:8080/user/get/avl/slots", {
-//       method: "POST",
-//       body: JSON.stringify({
-//         "branch_id": parseInt(selectedOption),
-//         "date": formatDate(selectedDate)
-//       }),
-//     })
-//       .then((res) => {
-//         if (!res.ok) {
-//           throw new Error("Network response was not ok");
-//         }
-//         return res.json();
-//       })
-//       .then((data) => {
-//         setAvailableSlots(data);
-//         setLoading(false);
-//         setShowSlotBooking(true); // Show SlotBooking after data is fetched
-//       })
-//       .catch((error) => {
-//         console.error("Error while fetching data:", error);
-//       });
-//   };
-//   console.log(availableSlots);
-
-//   useEffect(() => {
-//     fetch("http://localhost:8080/admin/active/branch")
-//       .then((res) => res.json())
-//       .then((data) => {
-//         setDropdownOptions(data.data);
-//       });
-//   }, []);
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-
-//     console.log("Booking Date (dd/mm/yyyy):", formatDate(selectedDate));
-//     console.log("Selected Option:", selectedOption);
-
-//     // Call the Postdata function to fetch data and update availableSlots
-//     Postdata();
-//     setSelectedDate(null);
-//     setSelectedOption("");
-//   };
-
-//   const formatDate = (date) => {
-//     if (date) {
-//       const day = date.getDate();
-//       const month = date.getMonth() + 1;
-//       const year = date.getFullYear();
-//       return `${day.toString().padStart(2, "0")}-${month.toString().padStart(2, "0")}-${year}`;
-//     }
-//     return "";
-//   };
-
-//   return (
-//     <div className="booking-form-container">
-//       <h2>Book Now</h2>
-//       <form onSubmit={handleSubmit}>
-//         <label>
-//           Select Option:
-//           <select
-//             value={selectedOption}
-//             onChange={(e) => setSelectedOption(e.target.value)}
-//           >
-//             <option value="">Select Branch</option>
-//             {dropdownOptions.map((option) => (
-//               <option key={option.ID} value={option.ID}>
-//                 {option.Branch_name}
-//               </option>
-//             ))}
-//           </select>
-//         </label>
-//         <br />
-//         <label>
-//           Booking Date:
-//           <DatePicker
-//             placeholderText="Select Date"
-//             selected={selectedDate}
-//             onChange={(date) => setSelectedDate(date)}
-//             dateFormat="dd-MM-yyyy"
-//           />
-//         </label>
-//         <br />
-//         <button type="submit">Submit</button>
-//         <Link to={"/"}>
-//           <button type="button">Close</button>
-//         </Link>
-//       </form>
-//       {showSlotBooking && (
-//         <SlotBooking availableSlots={availableSlots} loading={loading} />
-//       )}
-//     </div>
-//   );
-// }
-
-// export default BookingForm;
-
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import DatePicker from "react-datepicker";
@@ -128,8 +14,6 @@ function BookingForm() {
   const [showSlotBooking, setShowSlotBooking] = useState(false);
   const [errors, setErrors] = useState({});
 
-  
-
   const Postdata = () => {
     var payload = JSON.stringify({
       "branch_id": parseInt(selectedOption),
@@ -140,7 +24,7 @@ function BookingForm() {
       method: "POST",
       body: payload,
     })
-    
+
       .then((res) => {
         console.log(res);
         if (!res.ok) {
@@ -214,7 +98,6 @@ function BookingForm() {
     }
     return "";
   };
-  
 
   return (
     <div className="booking-form-container">
@@ -266,3 +149,59 @@ function BookingForm() {
 
 export default BookingForm;
 
+// import React, { useRef, useState } from "react";
+// // Import Swiper React components
+// import { Swiper, SwiperSlide } from "swiper/react";
+
+// // Import Swiper styles
+// import "swiper/css";
+// import "swiper/css/pagination";
+// import "swiper/css/navigation";
+
+// // import './styles.css';
+
+// // import required modules
+// import { Pagination, Navigation } from "swiper/modules";
+
+// function BookingForm() {
+//   return (
+//     <div className="container">
+//   <div className="row">
+//     <div className="col-md-5">
+//       <Swiper
+//         slidesPerView={1}
+//         spaceBetween={30}
+//         loop={true}
+//         pagination={{
+//           clickable: true,
+//         }}
+//         navigation={true}
+//         modules={[Pagination, Navigation]}
+//         className="mySwiper"
+//       >
+//         <SwiperSlide>Slide 1</SwiperSlide>
+//         <SwiperSlide>Slide 2</SwiperSlide>
+//         {/* ... (up to Slide 9) */}
+//       </Swiper>
+//     </div>
+
+//     <div className="col-md-7">
+//       <div className="d-flex flex-column justify-content-between h-100 pb--50-mob">
+//         <div className="row">
+//           <div className="col-8">
+//             <div className='d-flex justify-content-between align-items-center mt--5 mt--10-mob mb--15 mb--10-mob'>
+//               <div className="style_exclusive-badge__338Sr">
+//                 <div className="style_round-wrapper__1Pm1_">T</div>
+//                 "EXCLUSIVE"
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+
+//   );
+// }
+// export default BookingForm;
